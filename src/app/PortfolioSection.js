aaf95f100bd4a5147845d891ed11cd4c2f63d6f5
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-06-25 22:03:25 +0200

    Expanded portfolio section

--- src/app/PortfolioSection.js
+++ src/app/PortfolioSection.js
@@ -9,11 +9,57 @@ export const PortfolioSection = () => {
             </section>
             <section className={'portfolio-content-section'}>
                 <div className={'portfolio-projects-grid'}>
-                    <div className={'portfolio-project-photo'}>
-
-                    </div>
+                    <div className={'portfolio-project-photo'}
+                         style={{backgroundImage: `url(${process.env.PUBLIC_URL}/theme/images/mockup-trading.svg)`}}/>
                     <div className={'portfolio-project-text'}>
-
+                        <p className={'project-name'}>Trading App</p>
+                        <p className={'project-subtitle'}>Interaktywna giełda w Twojej kieszeni</p>
+                        <p className={'project-description'}>
+                            Zaprojektowana w widokach landscape i portrait, aplikacja umożliwia intuicyjne dodawanie,
+                            edycję i modyfikację linii trendu. Dzięki nowoczesnemu designowi i zaawansowanym funkcjom
+                            korzystanie z platformy staje się wyjątkowo proste i przyjemne, zapewniając najwyższy poziom
+                            funkcjonalności.
+                        </p>
+                        <button className={'see-more-button'}>Zobacz więcej ></button>
+                    </div>
+                    <div className={'portfolio-project-photo'}
+                         style={{backgroundImage: `url(${process.env.PUBLIC_URL}/theme/images/mockup-active.svg)`}}/>
+                    <div className={'portfolio-project-text'}>
+                        <p className={'project-name'}>Trading App</p>
+                        <p className={'project-subtitle'}>Interaktywna giełda w Twojej kieszeni</p>
+                        <p className={'project-description'}>
+                            Zaprojektowana w widokach landscape i portrait, aplikacja umożliwia intuicyjne dodawanie,
+                            edycję i modyfikację linii trendu. Dzięki nowoczesnemu designowi i zaawansowanym funkcjom
+                            korzystanie z platformy staje się wyjątkowo proste i przyjemne, zapewniając najwyższy poziom
+                            funkcjonalności.
+                        </p>
+                        <button className={'see-more-button'}>Zobacz więcej ></button>
+                    </div>
+                    <div className={'portfolio-project-photo'}
+                         style={{backgroundImage: `url(${process.env.PUBLIC_URL}/theme/images/mockup-find-trip.svg)`}}/>
+                    <div className={'portfolio-project-text'}>
+                        <p className={'project-name'}>Trading App</p>
+                        <p className={'project-subtitle'}>Interaktywna giełda w Twojej kieszeni</p>
+                        <p className={'project-description'}>
+                            Zaprojektowana w widokach landscape i portrait, aplikacja umożliwia intuicyjne dodawanie,
+                            edycję i modyfikację linii trendu. Dzięki nowoczesnemu designowi i zaawansowanym funkcjom
+                            korzystanie z platformy staje się wyjątkowo proste i przyjemne, zapewniając najwyższy poziom
+                            funkcjonalności.
+                        </p>
+                        <button className={'see-more-button'}>Zobacz więcej ></button>
+                    </div>
+                    <div className={'portfolio-project-photo'}
+                         style={{backgroundImage: `url(${process.env.PUBLIC_URL}/theme/images/mockup-blood-donations.svg)`}}/>
+                    <div className={'portfolio-project-text'}>
+                        <p className={'project-name'}>Trading App</p>
+                        <p className={'project-subtitle'}>Interaktywna giełda w Twojej kieszeni</p>
+                        <p className={'project-description'}>
+                            Zaprojektowana w widokach landscape i portrait, aplikacja umożliwia intuicyjne dodawanie,
+                            edycję i modyfikację linii trendu. Dzięki nowoczesnemu designowi i zaawansowanym funkcjom
+                            korzystanie z platformy staje się wyjątkowo proste i przyjemne, zapewniając najwyższy poziom
+                            funkcjonalności.
+                        </p>
+                        <button className={'see-more-button'}>Zobacz więcej ></button>
                     </div>
                 </div>
             </section>
