2ff8d43a02cd6304ca1382ec5d85e4123dc8d0f4
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-06-27 08:18:05 +0200

    Adjusted structure and styles to prepare for RWD changes

--- src/app/Project.js
+++ src/app/Project.js
@@ -3,15 +3,17 @@ import {SeeMoreBtn} from "./SeeMoreBtn";
 
 export const Project = ({imageName, name, subtitle, description, url}) => {
     return (
-        <>
-            <div className={'portfolio-project-photo'}
-                 style={{backgroundImage: `url(${process.env.PUBLIC_URL}/theme/images/${imageName})`}}/>
+        <div className={'portfolio-project'}>
+            <div className={'project-photo-wrapper'}>
+                <div className={'portfolio-project-photo'}
+                     style={{backgroundImage: `url(${process.env.PUBLIC_URL}/theme/images/${imageName})`}}/>
+            </div>
             <div className={'portfolio-project-text'}>
                 <p className={'project-name'}>{name}</p>
                 <p className={'project-subtitle'}>{subtitle}</p>
                 <p className={'project-description'}>{description}</p>
                 <SeeMoreBtn url={url}/>
             </div>
-        </>
+        </div>
     );
 }
--- src/routing/Router.js
+++ src/routing/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../index.css'
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import {Main} from "../app/Main";
 import {TradingApp} from "../app/svg-projects/TradingApp";
 import {ProjectActive} from "../app/svg-projects/ProjectActive";
@@ -16,6 +16,7 @@ export const Router = () => {
                 <Route path={'/project-active'} element={<ProjectActive />}/>
                 <Route path={'/trip-finder'} element={<TripFinder />}/>
                 <Route path={'/blood-donation'} element={<BloodDonation />}/>
+                <Route path='*' element={<Navigate to="/" />} />
             </Routes>
         </BrowserRouter>
     )
