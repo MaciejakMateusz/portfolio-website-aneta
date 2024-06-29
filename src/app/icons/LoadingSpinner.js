2e10d43a5cbf4a3cc2a7c8043a11672714943c62
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-06-29 16:28:41 +0200

    Centered spinner as it behaved differently on different devices

--- src/app/icons/LoadingSpinner.js
+++ src/app/icons/LoadingSpinner.js
@@ -3,7 +3,9 @@ import React from 'react';
 export const LoadingSpinner = () => {
     return (
         <div className="spinner-container">
-            <div className="spinner"/>
+            <div className="spinner-wrapper">
+                <div className="spinner"/>
+            </div>
         </div>
     );
 };
