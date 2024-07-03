54dbcd3f2946c76a4ed05c40ae9e4d4438853bd2
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-07-03 17:46:39 +0200

    Added certificates along with new dialog window

---
+++ src/app/CertificateDialog.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const CertificateDialog = ({fileName, setHidden, hidden, orientation}) => {
+
+    const hideDialog = () => {
+        setHidden(true);
+    }
+
+    return (
+        <div className={`dialog-overlay ${hidden ? '' : 'show'}`}
+             id="dialogOverlay"
+             onClick={hideDialog}>
+            <div className="dialog" onClick={hideDialog}>
+                <div className="dialog-body" onClick={hideDialog}>
+                    <img className={`certificate-dialog ${orientation}`}
+                         alt={'Certificate'}
+                         src={`${process.env.PUBLIC_URL}/theme/certificates/${fileName}`}/>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/experience/ExperienceSection.js
+++ src/app/experience/ExperienceSection.js
@@ -1,9 +1,19 @@
-import React, {forwardRef} from "react";
+import React, {forwardRef, useState} from "react";
 import {useTranslation} from "react-i18next";
 import {ExperienceBrand} from "./ExperienceBrand";
+import {CertificateDialog} from "../CertificateDialog";
 
 export const ExperienceSection = forwardRef((props, ref) => {
     const {t} = useTranslation();
+    const [dialogHidden, setDialogHidden] = useState(true);
+    const [fileName, setFileName] = useState('');
+    const [orientation, setOrientation] = useState('horizontal');
+
+    const renderCertificateDialog = (fileName, orientation) => {
+        setDialogHidden(false);
+        setFileName(fileName);
+        setOrientation(orientation)
+    }
 
     return (
         <section className={'experience-section'} id={'experience'} ref={ref}>
@@ -13,6 +23,30 @@ export const ExperienceSection = forwardRef((props, ref) => {
                 <ExperienceBrand wrapperType={'a'} imageName={'softie.gif'} alt={'Softie'}/>
                 <ExperienceBrand wrapperType={'b'} imageName={'akademiaUXUI.png'} alt={'Akademia UXUI'}/>
             </div>
+            <div className={'certificates-grid'}>
+                <div className={'certificate a'}>
+                    <img className={'certificate-image horizontal'}
+                         alt={'Certificate'}
+                         src={`${process.env.PUBLIC_URL}/theme/certificates/certificate-professional.jpeg`}
+                         onClick={() => renderCertificateDialog('certificate-professional.jpeg', 'horizontal')}/>
+                </div>
+                <div className={'certificate b'}>
+                    <img className={'certificate-image vertical'}
+                         alt={'Certificate'}
+                         src={`${process.env.PUBLIC_URL}/theme/certificates/diploma-professional.jpeg`}
+                         onClick={() => renderCertificateDialog('diploma-professional.jpeg', 'vertical')}/>
+                </div>
+                <div className={'certificate c'}>
+                    <img className={'certificate-image horizontal'}
+                         alt={'Certificate'}
+                         src={`${process.env.PUBLIC_URL}/theme/certificates/ux-certificate.jpg`}
+                         onClick={() => renderCertificateDialog('ux-certificate.jpg', 'horizontal')}/>
+                </div>
+            </div>
+            {dialogHidden ? null : <CertificateDialog fileName={fileName}
+                                                      setHidden={setDialogHidden}
+                                                      hidden={dialogHidden}
+                                                      orientation={orientation}/>}
         </section>
     );
 });
